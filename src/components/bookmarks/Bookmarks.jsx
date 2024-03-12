fb8e7f95e452fa9a94d45618d0eb3b26a896ce03
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-13 00:03:20 +0600

    eight commit

--- src/components/bookmarks/Bookmarks.jsx
+++ src/components/bookmarks/Bookmarks.jsx
@@ -1,11 +1,22 @@
 
 
-const Bookmarks = ({bookmarks}) => {
-    return (
-        <div className="w-1/3">
-             <h2 className="text-3xl">{bookmarks.length}</h2>
-        </div>
-    );
-};
+import React from 'react'
+import PropTypes from 'prop-types'
+import Bookmark from '../bookmark/Bookmark'
 
-export default Bookmarks;
+const Bookmarks = ({bookmarks}) => {
+  return (
+    <div className="w-1/3 bg-gray-400 ml-4 mt-2 p-2">
+             <h2 className="text-3xl text-center ">Bookmarked Blogs: {bookmarks.length}</h2>
+             {
+                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark> )
+             }
+        </div>
+  )
+}
+
+Bookmarks.propTypes = {
+    bookmarks:PropTypes.array
+}
+
+export default Bookmarks
