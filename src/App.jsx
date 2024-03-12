2f7822814b64e80d012fbaf323e699c909c0451e
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-12 20:58:53 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header/Header'
+import Blogs from './components/blogs/Blogs'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-       
-      <h1 className='bg-red-600'>Vite + React</h1>
+       <Header></Header>
+       <Blogs></Blogs>
       
     </>
   )
 }
 
 export default App
+
 
