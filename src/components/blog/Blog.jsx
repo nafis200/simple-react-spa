c6dfc81c7a92fe98cd3994ebada5c7b7ead6de4c
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-12 23:02:39 +0600

    fivth commit

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,10 +1,27 @@
 
 import PropTypes from 'prop-types'
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {title,cover,author,img,reading_time,posted_date,hashtag} = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`cover picture of title`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img src={author} alt="" />
+                    <div>
+                        <h3 className=' text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p>{
+                hashtag.map((hash,idx) => <span key={idx}><a href="">#{hash}</a></span>  )   
+            }</p>
         </div>
     );
 };
